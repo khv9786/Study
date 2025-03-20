7b3b87a880fc9531fdc2b96a23ed08e7b9f0c909
김현빈 <96505736+khv9786@users.noreply.github.com>
2025-03-20 14:57:11 +0900

    [N+1] @EntityGraph 어노테이션 사용으로 해결

--- JPA/n+1_Problem/src/main/java/com/example/study/domain/customer/CustomerRepository.java
+++ JPA/n+1_Problem/src/main/java/com/example/study/domain/customer/CustomerRepository.java
@@ -1,18 +1,19 @@
 package com.example.study.domain.customer;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    //1. 방법 FETCH JOIN
-    //고객과 연관된 주문을 한번에 가져오는 방식을 선택.
-    //한번에 모든 데이터를 가져오므로 빠르고, 추가적인 쿼리 X
-    //단 조인되는 데이터가 너무 많으면 성능 저하 우려.
-    //또한 여러 개 사용이 어려워 복잡해질 수 있는 단점이 존재.
-    @Query("SELECT c FROM Customer c JOIN FETCH c.orders")
+    //2. 방법 @EntityGraph 사용
+    //간단한 어노테이션을 통해 FETCH JOIN을 편하게 사용할 수 있다.
+    //JPQL과도 함께 사용 할 수 있으나 left outer join만 지원한다.
+    //또한 attribute를 직접 지정해야하므로 유연성이 떨어진다.
+    //예제에서는 "orders"처럼 지정해야한다.
+    @EntityGraph(attributePaths = "orders")
     List<Customer> findAllWithOrders();
 }
 
